12477243db8bce011230321f99907a0df6aa0512
Tylor Moore <mooretylor28@gmail.com>
2022-03-28 13:04:11 -0400

    arrays and lists exercise

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace ArraysAndLists
 {
     class Program
@@ -7,26 +7,52 @@ namespace ArraysAndLists
         static void Main(string[] args)
         {
             //TODO:
+            int[] myArray1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-            // Create an int Array and populate numbers 1-10
+            var evens = new List<int>();
 
-            /* Create two Lists of type int.
-             * Name one List "evens"
-             * Name the other List "odds"
-             */
+            var odds = new List<int>();
+            for (int i =0; i < myArray1.Length; i++)
+            {
+                if (myArray1[i] % 2 == 0)
+                {
+                    evens.Add(myArray1[i]);
+                }
+                else
+                {
+                    odds.Add(myArray1[i]);
+                }
+            }
+            foreach (int i in evens)
+            {
+                Console.WriteLine(i);
+            }
+            foreach (int i in odds)
+            {
+                Console.WriteLine(i);
+            }
 
-            /* Using either a foreach or for loop,
-             * nest an if statement to check to see
-             *  if a number is even or odd.
-             * Then add those numbers to either the evens List
-             * or the odds List
-             */
-
-            /* Now using foeach or for loops,
-             * display each List of even and odd numbers
-             *
-             * Try to be creative in your display
-             */
         }
+        // Create an int Array and populate numbers 1-10
+        // 
+         //Create two Lists of type int 
+
+        // * Name one List "evens"
+
+        // * Name the other List "odds"
+
+        //Using either a foreach or for loop,
+
+        // * nest an if statement to check to see
+
+        //*  if a number is even or odd.
+
+        // * Then add those numbers to either the evens List
+        //* or the odds List
+        /* Now using foeach or for loops,
+         * display each List of even and odd numbers
+         *
+         * Try to be creative in your display
+         */
     }
 }
