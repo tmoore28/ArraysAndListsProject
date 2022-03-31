14c0fc67ed999a1b0481b7abd93618e87fa5981f
Tylor Moore <mooretylor28@gmail.com>
2022-03-31 16:24:53 -0400

    initial commit

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -33,18 +33,12 @@ namespace ArraysAndLists
             }
 
         }
-        // Create an int Array and populate numbers 1-10
-        // 
+        // Create an int Array and populate numbers 1-10 
          //Create two Lists of type int 
-
         // * Name one List "evens"
-
         // * Name the other List "odds"
-
         //Using either a foreach or for loop,
-
         // * nest an if statement to check to see
-
         //*  if a number is even or odd.
 
         // * Then add those numbers to either the evens List
